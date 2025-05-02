4bdf88184d117d61f7452e8963d11a796077077f
Aakaive <wonseok031@gmail.com>
2025-05-02 22:09:22 +0900

    chore : create project

---
+++ src/main/java/com/aaka/web_scheduler/WebSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.aaka.web_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aaka/web_scheduler/WebSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaka.web_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
